12276dc75200813d3915d050a6c42aafc7a30de9
kjxbyz <kjxbyz888@outlook.com>
2024-05-30 22:14:19 +0800

    initial commit

---
+++ Casks/faforever.rb
@@ -0,0 +1,39 @@
+# Documentation: https://docs.brew.sh/Cask-Cookbook
+#                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+cask "faforever" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "0.0.0+17"
+  # https://docs.brew.sh/Cask-Cookbook#stanza-sha256
+  sha256 :no_check
+
+  # https://docs.brew.sh/Cask-Cookbook#stanza-url
+  # https://docs.brew.sh/Cask-Cookbook#when-url-and-homepage-domains-differ-add-verified
+  url "https://github.com/cyf/faforever/releases/download/v#{version}/FaForever_#{version}_#{arch}.dmg",
+      verified: "https://github.com/cyf/faforever/"
+  name "FaForever"
+  desc "FaForever."
+  homepage "https://chenyifaer.com/faforever"
+
+  # Documentation: https://docs.brew.sh/Brew-Livecheck
+  livecheck do
+    url :url
+    strategy :git do |tags|
+      tags.map { |tag|
+        match = tag&.match(/^v?(\d+(?:\.\d+)+)(\+\d+)?$/i)
+        next if match.blank?
+        tag
+      }.compact
+    end
+  end
+
+  # https://docs.brew.sh/Cask-Cookbook#stanza-depends_on
+  depends_on macos: ">= :catalina"
+
+  # https://docs.brew.sh/Cask-Cookbook#stanza-app
+  app "FaForever.app"
+
+  # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
+  zap trash: []
+end
---
+++ README.md
@@ -0,0 +1,26 @@
+# homebrew-faforever
+
+[![brew test-bot](https://github.com/cyf/homebrew-faforever/actions/workflows/tests.yml/badge.svg?branch=main)](https://github.com/cyf/homebrew-faforever/actions/workflows/tests.yml)
+
+## Install
+
+`brew install cyf/faforever/faforever`
+
+Or `brew tap cyf/faforever` and then `brew install faforever`.
+
+Or, in a [`brew bundle`](https://github.com/Homebrew/homebrew-bundle) `Brewfile`:
+
+```ruby
+tap "cyf/faforever"
+brew "faforever"
+```
+
+## Debug
+
+`brew livecheck --debug <formula>|<cask>`
+
+## Documentation
+
+Your taps are Git repositories located at `$(brew --repository)/Library/Taps`
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
